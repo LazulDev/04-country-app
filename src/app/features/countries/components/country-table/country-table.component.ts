28f0a00c5b0b2ff38060df98b38d8d2e689932c3
María Sánchez Carvajal <maria.carvajal@galpsolar.com>
2024-04-26 12:16:39 +0200

    feat: redirect to country page

--- src/app/features/countries/components/country-table/country-table.component.ts
+++ src/app/features/countries/components/country-table/country-table.component.ts
@@ -1,11 +1,16 @@
+import {DecimalPipe} from "@angular/common";
 import {Component, Input} from '@angular/core';
+import {RouterLink} from "@angular/router";
 import {Country} from "../../domain/country";
 import {CountryDto} from "../../infrastructure/country-dto";
 
 @Component({
   selector: 'countries-country-table',
   standalone: true,
-  imports: [],
+  imports: [
+    DecimalPipe,
+    RouterLink
+  ],
   templateUrl: './country-table.component.html',
   styleUrl: './country-table.component.css'
 })
